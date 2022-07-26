bae5e2feb4c40cba0d596ac472dfcc329cc3a6b3
armando salamanca ayon <asalamanca.ayon@gmail.com>
2022-07-26 18:45:52 -0500

    fix routes when you refresh the page

--- index.js
+++ index.js
@@ -19,6 +19,10 @@ app.use(express.json());
 
 routerApi(app);
 
+app.get("*", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
 //listen requets
 app.listen(PORT, () => {
   console.log(`app listen in [PORT]:${PORT}`);
